bf7eeb495a5a08d79da79b247f10da02af025519
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2022-10-20 08:21:59 +0200

    inserito commenti

--- js/libreria.js
+++ js/libreria.js
@@ -1,32 +1,41 @@
+//genera un numero random
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
 }
 
+//nasconde un elemento se non  già nascosto
 function hiddenElement(elemento){
     if(!elemento.classList.contains('my-d-none')){
         elemento.classList.add('my-d-none');
     }
 }
+
+//mostro un elemento se non è già in mostra
 function showElement(elemento){
     if(elemento.classList.contains('my-d-none')){
         elemento.classList.remove('my-d-none');
     }
 }
 
-function cancelResult(){
-    const result = document.querySelector('.result');
-    result.innerHTML = '';
+//resetto l'inerno di un div
+function cancelResult(div){
+    div.innerHTML = '';
 }
 
+//funzione che a loop con un timer printa del contenuto in HTML e gli attribuisce opacità
 function myLoop(array,i,time,div) {         
     setTimeout(function() {   
         div.innerHTML = `${array[i]}`;  
         i++;                  
-        if (i < array.length) {          
+        if (i < array.length) { 
+            div.classList.add('opacity-1');
+            setTimeout(function(){div.classList.remove('opacity-1')},time*0.7);
             myLoop(array,i,time,div);             
         }
         if (i == array.length){
-            setTimeout(cancelResult,time);
+            div.classList.add('opacity-1');
+            setTimeout(function(){div.classList.remove('opacity-1')},time*0.7);
+            setTimeout(cancelResult,time,div);
         }
     }, time)
 }
--- js/script.js
+++ js/script.js
@@ -17,11 +17,15 @@ btnRetry.addEventListener('click', retry);
 let numbersGenerated = [];
 let userNumbers = [];
 
+//funzione che resetta alle impostazioni iniziali del gioco
 function retry(){
+    //resetta i timer delle funzione con delay
     clearTimeout(fun1);
     clearTimeout(fun2);
+    //tolgo il div
     const result = document.querySelector('.result');
-    result.remove();
+    game.removeChild(result);
+    //nascondo mostro e resetto i vari campi
     showElement(userInput);
     showElement(btnPlay);
     showElement(title);
@@ -34,54 +38,64 @@ function retry(){
     message.innerHTML = '';
     message.classList.remove('text-white');
     message.classList.remove('text-danger');
-    
 }
 
 function handleClick(){
 
     if(this.value == 'Play'){
-        const result = addElementClassHTML('div', 'result my-d-none', game);
-        message.classList.remove('text-white');
-        message.classList.remove('text-danger');
-        message.innerHTML = '';
-        result.innerHTML = '';
+        //prendo l'input
         const numbersToBeGenerated = parseInt(userInput.value);
+
+        //controllo sull'input
         if(isNaN(numbersToBeGenerated) || numbersToBeGenerated>80){
             showElement(message);
             message.classList.add('text-danger');
             message.innerHTML = 'Devi inserire un numero minore di 80!'
             return;
         }
-        // let numbersToBeGenerated = 5;
+        //creo e appendo il div su cui faccio apparire i numeri
+        const result = addElementClassHTML('div', 'result my-d-none', game);
+
+        //in caso non venga premuto retry per resettare
+        message.classList.remove('text-white');
+        message.classList.remove('text-danger');
+        message.innerHTML = '';
+        result.innerHTML = '';
+
+        // genero la lista di numeri casuali diversi
         const minRandom = 0;
         const maxRandom = 100;
-    
         while(numbersGenerated.length < numbersToBeGenerated){
             const randomNumber = getRandomNumber(minRandom, maxRandom);
             if(!numbersGenerated.includes(randomNumber)){
                 numbersGenerated.push(randomNumber);
             }
         }
+        //nascondo e mostro gli elementi
         hiddenElement(userInput);
         hiddenElement(btnPlay);
         hiddenElement(title);
         showElement(result);
         showElement(btnRetry);
+
+        //debug
         console.log(numbersGenerated);
+        //stampo i numeri in HTMl lentamente
         let i = 0;
-        const time = 1500;
-        // const time = 250;
+        const time = 2000;
         myLoop(numbersGenerated,i,time,result);
     
         const timeLoop = (time * (numbersToBeGenerated + 1) + 500);
 
         this.value = 'Enter';
         userInput.value = '';
+        //mostro gli elementi al timing giusto
         fun1 = setTimeout( showElement , timeLoop, userInput);
         fun2 = setTimeout( showElement , timeLoop, btnPlay);
     }else if(this.value == 'Enter'){
         message.innerHTML = ''
         const userNumber = parseInt(userInput.value);
+        //controllo l'input
         if(isNaN(userNumber)){
             showElement(message);
             message.classList.add('text-danger');
@@ -89,15 +103,14 @@ function handleClick(){
             return;
         }
         userInput.value = '';
+        //salvo l'input
         userNumbers.push(userNumber);
-
+        //quando l'utente inserisce la quantità di numeri mostro la verifica
         if(userNumbers.length == numbersGenerated.length){
             hiddenElement(userInput);
             btnPlay.value = 'Verify';
         }
     }else if(this.value == 'Verify'){
-        btnPlay.classList.add('btn-dark');
-        btnPlay.classList.remove('btn-success');
         hiddenElement(btnPlay);
         checkVictory();
     }
@@ -105,16 +118,23 @@ function handleClick(){
 }
 
 function checkVictory(){
+    //prendo e mostro il messaggio di vittoria
     const result = document.querySelector('.result');
+    result.classList.add('opacity-1');
+
+    //mostro le due liste
     showElement(message);
-    message.classList.add('text-white')
+    message.classList.remove('text-danger');
+    message.classList.add('text-white');
+
+    //controllo vittoria e stampo i messaggi
     let victory = true;
     for(let i = 0; i < userNumbers.length && victory; i++){
         if(!numbersGenerated.includes(userNumbers[i])){
             victory = false;
         }
     }
-    message.innerHTML = `Lista generata: ${numbersGenerated.toString()} <br> Lista inserita: ${userNumbers.toString()} <br>` 
+    message.innerHTML = `Lista inserita: ${userNumbers.toString()} <br> Lista generata: ${numbersGenerated.toString()} <br>` 
     if(victory){
         result.innerHTML = 'Hai Vinto!'
     }else{
